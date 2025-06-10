27957f7bb788df125f5f0716e5b4e95ba8ba0498
christdev37 <juniormalcom912@gmail.com>
2025-06-10 12:28:52 +0100

    update our domain to conform with jpa standard

--- src/main/java/com/christdev/database/domain/Author.java
+++ src/main/java/com/christdev/database/domain/Author.java
@@ -1,6 +1,7 @@
 package com.christdev.database.domain;
 
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,8 +11,13 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Entity // Object than be used by spring data JPA
+@Table(name = "authors") //we r mapping it to the authors table
 public class Author {
 
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "author_id_seq")
     private Long id;
     private String name;
     private Integer age;
