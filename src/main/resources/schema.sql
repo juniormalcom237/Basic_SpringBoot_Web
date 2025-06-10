3de8887a348833f476059be216f0eb231b1f22a9
christdev37 <juniormalcom912@gmail.com>
2025-06-10 11:50:40 +0100

    DELETED THE SQL Schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -9,30 +9,30 @@
 --
 --     constraint "widgets_pkey" PRIMARY KEY ("id")
 -- );
-
-
-DROP TABLE IF EXISTS "books";
-DROP TABLE IF EXISTS "authors";
-
-
-drop sequence IF exists authors_id_seq;
-create sequence authors_id_seq increment by 1 maxvalue 922337203654775807 cache 1;
-
-
-create TABLE "authors" (
-    "id" bigint default nextval('authors_id_seq') NOT NULL,
-    "name" text,
-    "age" integer,
-    CONSTRAINT "authors_pkey" PRIMARY KEY ("id")
-);
-
-create table "books" (
-  "isbn" text not null,
-  "title" text,
-  "author_id"  bigint,
-  constraint "books_pkey" PRIMARY KEY ("isbn"),
-  constraint "fk_author" FOREIGN KEY (author_id)
-  REFERENCES authors(id)
-);
-
-
+--
+--
+-- DROP TABLE IF EXISTS "books";
+-- DROP TABLE IF EXISTS "authors";
+--
+--
+-- drop sequence IF exists authors_id_seq;
+-- create sequence authors_id_seq increment by 1 maxvalue 922337203654775807 cache 1;
+--
+--
+-- create TABLE "authors" (
+--     "id" bigint default nextval('authors_id_seq') NOT NULL,
+--     "name" text,
+--     "age" integer,
+--     CONSTRAINT "authors_pkey" PRIMARY KEY ("id")
+-- );
+--
+-- create table "books" (
+--   "isbn" text not null,
+--   "title" text,
+--   "author_id"  bigint,
+--   constraint "books_pkey" PRIMARY KEY ("isbn"),
+--   constraint "fk_author" FOREIGN KEY (author_id)
+--   REFERENCES authors(id)
+-- );
+--
+--
